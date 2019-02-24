de54ced512f7ea14d1f80a8d8679d535fbd9c6ee
Ed Schouten <ed.schouten@prodrive-technologies.com>
2019-02-24 10:02:45 +0100

    Make event logging somewhat work?

--- WORKSPACE
+++ WORKSPACE
@@ -3,6 +3,13 @@ workspace(name = "com_github_buildbarn_bb_event_service")
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 http_archive(
+    name = "io_bazel_rules_docker",
+    sha256 = "aed1c249d4ec8f703edddf35cbe9dfaca0b5f5ea6e4cd9e83e99f3b0d1136c3d",
+    strip_prefix = "rules_docker-0.7.0",
+    urls = ["https://github.com/bazelbuild/rules_docker/archive/v0.7.0.tar.gz"],
+)
+
+http_archive(
     name = "io_bazel_rules_go",
     sha256 = "ade51a315fa17347e5c31201fdc55aa5ffb913377aa315dceb56ee9725e620ee",
     url = "https://github.com/bazelbuild/rules_go/releases/download/0.16.6/rules_go-0.16.6.tar.gz",
@@ -14,6 +21,13 @@ http_archive(
     urls = ["https://github.com/bazelbuild/bazel-gazelle/releases/download/0.16.0/bazel-gazelle-0.16.0.tar.gz"],
 )
 
+http_archive(
+    name = "com_github_bazelbuild_bazel",
+    sha256 = "6860a226c8123770b122189636fb0c156c6e5c9027b5b245ac3b2315b7b55641",
+    urls = ["https://github.com/bazelbuild/bazel/releases/download/0.22.0/bazel-0.22.0-dist.zip"],
+    patches = ["//:patches/com_github_bazelbuild_bazel/build_event_stream.diff"],
+)
+
 load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
 
 git_repository(
@@ -21,6 +35,10 @@ git_repository(
     commit = "91e95ab36b90259bea60e5b525182694e7450349",
     remote = "https://github.com/buildbarn/bb-storage.git",
 )
+
+load("@io_bazel_rules_docker//repositories:repositories.bzl", container_repositories = "repositories")
+
+container_repositories()
 
 load("@io_bazel_rules_go//go:def.bzl", "go_register_toolchains", "go_rules_dependencies")
 
--- cmd/bb_event_logger/main.go
+++ cmd/bb_event_logger/main.go
@@ -1,32 +1,123 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
+	"sync"
 
+	buildeventstream "github.com/bazelbuild/bazel/src/main/java/com/google/devtools/build/lib/buildeventstream/proto"
+	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"github.com/buildbarn/bb-storage/pkg/blobstore"
+	"github.com/buildbarn/bb-storage/pkg/blobstore/configuration"
+	"github.com/buildbarn/bb-storage/pkg/util"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/matttproud/golang_protobuf_extensions/pbutil"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	build "google.golang.org/genproto/googleapis/devtools/build/v1"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type noopBuildEventServer struct {
+type streamState struct {
+	committedSequences int64
+	bazelBuildEvents   *bytes.Buffer
 }
 
-func (bes *noopBuildEventServer) PublishLifecycleEvent(ctx context.Context, in *build.PublishLifecycleEventRequest) (*empty.Empty, error) {
-	log.Printf("PublishLifecycleEvent 1: %#v", in)
-	log.Printf("PublishLifecycleEvent 2: %#v", in.BuildEvent.Event)
+type buildEventServer struct {
+	contentAddressableStorage blobstore.BlobAccess
+	instanceName              string
+
+	lock    sync.Mutex
+	streams map[string]*streamState
+}
+
+func (bes *buildEventServer) PublishLifecycleEvent(ctx context.Context, in *build.PublishLifecycleEventRequest) (*empty.Empty, error) {
+	// For now, completely ignore lifecycle events.
 	return &empty.Empty{}, nil
 }
 
-func (bes *noopBuildEventServer) PublishBuildToolEventStream(stream build.PublishBuildEvent_PublishBuildToolEventStreamServer) error {
+func (bes *buildEventServer) processBuildToolEvent(ctx context.Context, in *build.PublishBuildToolEventStreamRequest) (*build.PublishBuildToolEventStreamResponse, error) {
+	bes.lock.Lock()
+	defer bes.lock.Unlock()
+
+	key := proto.MarshalTextString(in.OrderedBuildEvent.StreamId)
+	state, ok := bes.streams[key]
+	if ok {
+		if in.OrderedBuildEvent.SequenceNumber < state.committedSequences+1 {
+			// Historical event that was retransmitted. Nothing to do.
+			return &build.PublishBuildToolEventStreamResponse{
+				StreamId:       in.OrderedBuildEvent.StreamId,
+				SequenceNumber: state.committedSequences,
+			}, nil
+		} else if in.OrderedBuildEvent.SequenceNumber > state.committedSequences+1 {
+			// Event from the future.
+			return nil, status.Error(codes.InvalidArgument, "Event has sequence number from the future")
+		}
+	} else {
+		if in.OrderedBuildEvent.SequenceNumber != 1 {
+			return nil, status.Error(codes.DataLoss, "Stream is not known by the server")
+		}
+		state = &streamState{
+			committedSequences: 0,
+			bazelBuildEvents:   bytes.NewBuffer(nil),
+		}
+		bes.streams[key] = state
+	}
+
+	switch buildEvent := in.OrderedBuildEvent.Event.Event.(type) {
+	case *build.BuildEvent_ComponentStreamFinished:
+		log.Print("BuildTool: ComponentStreamFinished: ", buildEvent.ComponentStreamFinished)
+		data := state.bazelBuildEvents.Bytes()
+		hash := sha256.Sum256(data)
+		digest, err := util.NewDigest(bes.instanceName, &remoteexecution.Digest{
+			Hash:      hex.EncodeToString(hash[:]),
+			SizeBytes: int64(len(data)),
+		})
+		log.Printf("Storing log as %s", digest)
+		if err != nil {
+			return nil, err
+		}
+		if err := bes.contentAddressableStorage.Put(
+			ctx, digest, digest.GetSizeBytes(),
+			ioutil.NopCloser(bytes.NewBuffer(data))); err != nil {
+			return nil, err
+		}
+		delete(bes.streams, key)
+	case *build.BuildEvent_BazelEvent:
+		log.Print("BuildTool: BazelEvent")
+		var bazelBuildEvent buildeventstream.BuildEvent
+		if err := ptypes.UnmarshalAny(buildEvent.BazelEvent, &bazelBuildEvent); err != nil {
+			return nil, err
+		}
+		if _, err := pbutil.WriteDelimited(state.bazelBuildEvents, &bazelBuildEvent); err != nil {
+			return nil, err
+		}
+	default:
+		log.Print("Received unknown BuildToolEvent")
+	}
+
+	state.committedSequences++
+	return &build.PublishBuildToolEventStreamResponse{
+		StreamId:       in.OrderedBuildEvent.StreamId,
+		SequenceNumber: state.committedSequences,
+	}, nil
+}
+
+func (bes *buildEventServer) PublishBuildToolEventStream(stream build.PublishBuildEvent_PublishBuildToolEventStreamServer) error {
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
@@ -35,13 +126,12 @@ func (bes *noopBuildEventServer) PublishBuildToolEventStream(stream build.Publis
 			log.Print(err)
 			return err
 		}
-		log.Printf("PublishBuildToolEventStream 1: %#v", in)
-		log.Printf("PublishBuildToolEventStream 2: %#v", in.OrderedBuildEvent.Event)
-
-		if err := stream.Send(&build.PublishBuildToolEventStreamResponse{
-			StreamId:       in.OrderedBuildEvent.StreamId,
-			SequenceNumber: in.OrderedBuildEvent.SequenceNumber,
-		}); err != nil {
+		response, err := bes.processBuildToolEvent(stream.Context(), in)
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		if err := stream.Send(response); err != nil {
 			log.Print(err)
 			return err
 		}
@@ -50,9 +140,16 @@ func (bes *noopBuildEventServer) PublishBuildToolEventStream(stream build.Publis
 
 func main() {
 	var (
+		blobstoreConfig  = flag.String("blobstore-config", "/config/blobstore.conf", "Configuration for blob storage")
 		webListenAddress = flag.String("web.listen-address", ":80", "Port on which to expose metrics")
 	)
 	flag.Parse()
+
+	// Storage access.
+	contentAddressableStorage, _, err := configuration.CreateBlobAccessObjectsFromConfig(*blobstoreConfig)
+	if err != nil {
+		log.Fatal("Failed to create blob access: ", err)
+	}
 
 	// Web server for metrics and profiling.
 	http.Handle("/metrics", promhttp.Handler())
@@ -65,7 +162,12 @@ func main() {
 		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
 		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
 	)
-	build.RegisterPublishBuildEventServer(s, &noopBuildEventServer{})
+	build.RegisterPublishBuildEventServer(s, &buildEventServer{
+		contentAddressableStorage: contentAddressableStorage,
+		instanceName:              "hello",
+
+		streams: map[string]*streamState{},
+	})
 	grpc_prometheus.EnableHandlingTimeHistogram()
 	grpc_prometheus.Register(s)
 	sock, err := net.Listen("tcp", ":8983")
