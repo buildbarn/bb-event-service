0bc490c9afafcbce239ff17db8e5f124aeeb5925
Ed Schouten <eschouten@apple.com>
2019-12-17 21:39:48 +0100

    Tidy up config file handling to be the same as for other bb_* apps

--- cmd/bb_event_service/main.go
+++ cmd/bb_event_service/main.go
@@ -7,8 +7,9 @@ import (
 	_ "net/http/pprof"
 	"os"
 
-	"github.com/buildbarn/bb-event-service/pkg/configuration"
+	"github.com/buildbarn/bb-event-service/pkg/proto/configuration/bb_event_service"
 	blobstore "github.com/buildbarn/bb-storage/pkg/blobstore/configuration"
+	"github.com/buildbarn/bb-storage/pkg/util"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	build "google.golang.org/genproto/googleapis/devtools/build/v1"
@@ -18,18 +19,17 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatal("Usage: bb-event-service bb-event-service.conf")
+		log.Fatal("Usage: bb_event_service bb_event_service.jsonnet")
 	}
-
-	eventServiceConfiguration, err := configuration.GetEventServiceConfiguration(os.Args[1])
-	if err != nil {
+	var configuration bb_event_service.ApplicationConfiguration
+	if err := util.UnmarshalConfigurationFromFile(os.Args[1], &configuration); err != nil {
 		log.Fatalf("Failed to read configuration from %s: %s", os.Args[1], err)
 	}
 
 	// Storage access.
 	contentAddressableStorage, actionCache, err := blobstore.CreateBlobAccessObjectsFromConfig(
-		eventServiceConfiguration.Blobstore,
-		int(eventServiceConfiguration.MaximumMessageSizeBytes))
+		configuration.Blobstore,
+		int(configuration.MaximumMessageSizeBytes))
 	if err != nil {
 		log.Fatal("Failed to create blob access: ", err)
 	}
@@ -37,7 +37,7 @@ func main() {
 	// Web server for metrics and profiling.
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
-		log.Fatal(http.ListenAndServe(eventServiceConfiguration.MetricsListenAddress, nil))
+		log.Fatal(http.ListenAndServe(configuration.MetricsListenAddress, nil))
 	}()
 
 	// RPC server.
@@ -54,7 +54,7 @@ func main() {
 	})
 	grpc_prometheus.EnableHandlingTimeHistogram()
 	grpc_prometheus.Register(s)
-	sock, err := net.Listen("tcp", eventServiceConfiguration.GrpcListenAddress)
+	sock, err := net.Listen("tcp", configuration.GrpcListenAddress)
 	if err != nil {
 		log.Fatal("Failed to create listening socket: ", err)
 	}
